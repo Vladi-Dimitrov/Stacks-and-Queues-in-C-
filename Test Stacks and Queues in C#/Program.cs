f2f69b45dccc0204fe5536b88df47d4d10f52f2c
Vladi-Dimitrov <v.dimitrov2403@gmail.com>
2024-12-11 13:48:28 +0200

    commit message

--- Test Stacks and Queues in C#/Program.cs
+++ Test Stacks and Queues in C#/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] input = Console.ReadLine();
+                 
+int N = int.Parse(input[0]); 
+int S = int.Parse(input[1]); 
+int X = int.Parse(input[2]); 
+
+int[] elements = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToArray();
+
+Stack<int> stack = new Stack<int>();
+
+for (int i = 0; i < N; i++)
+{
+    stack.Push(elements[i]);
+}
+
+for (int i = 0; i < S; i++)
+{
+    if (stack.Count > 0)
+    {
+        stack.Pop();
+    }
+}
+
+
+if (stack.Contains(X))
+{
+    Console.WriteLine("found");
+}
+else
+{
+
+    if (stack.Count == 0)
+    {
+        Console.WriteLine("nothing found");
+    }
+    else
+    {
+        Console.WriteLine(stack.Min());
+    }
+}
+
